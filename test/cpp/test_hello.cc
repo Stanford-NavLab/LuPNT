fa1f154e32173dbe9452706e2a4fbd1d8cb8bbfe
Guillem <gcasadesusvila@gmail.com>
2023-11-23 23:37:01 -0800

    Switched to catch2

--- test/cpp/test_hello.cc
+++ test/cpp/test_hello.cc
@@ -1,9 +1,19 @@
-#include <gtest/gtest.h>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
+#include <iostream>
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
+TEST_CASE("HelloTest", "[BasicAssertions]") {
   // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
+  REQUIRE_THAT("hello", !Catch::Matchers::Equals("world"));
   // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+  REQUIRE(7 * 6 == 42);
+
+  // print environment variable LUPNT_DATA_PATH
+  char *env = std::getenv("LUPNT_DATA_PATH");
+  if (env != NULL) {
+    std::cout << "LUPNT_DATA_PATH=" << env << '\n';
+  } else {
+    std::cout << "LUPNT_DATA_PATH is not set\n";
+  }
 }
