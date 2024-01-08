efbd328c2db04cee63e60372739f6a7fadf63af3
Guillem <gcasadesusvila@gmail.com>
2024-01-08 01:39:39 +0100

    Working on tests

--- pylupnt/pylupnt/__init__.py
+++ pylupnt/pylupnt/__init__.py
@@ -1,11 +1,16 @@
 try:
-    from .pylupnt_pybind import * # py2 py3 compatible
+    from .pylupnt_pybind import *  # py2 py3 compatible
+
+    # from . import plots, utils
 except Exception:
     # this was installed with as a python wheel
     from pylupnt_pybind import *
 
+    # from . import plots, utils
+
 try:
     import pkg_resources  # part of setuptools
+
     __version__ = pkg_resources.require("pylupnt")[0].version
 except Exception:
-    __version__ = '@PROJECT_VERSION@'
+    __version__ = "@PROJECT_VERSION@"
