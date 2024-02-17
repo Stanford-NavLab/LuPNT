0665604a24734595c0b2279120360b4ce61e1fa1
Guillem <gcasadesusvila@gmail.com>
2024-02-16 22:16:44 -0800

    Minor changes

--- scripts/dev_install_pylupnt.sh
+++ scripts/dev_install_pylupnt.sh
@@ -1,4 +1,4 @@
-/opt/homebrew/bin/cmake --build /Users/guillemcv/Development/NavLab/LuPNT/build --config Debug --target python-package -j 14
+cmake --build build --config Debug --target lupnt pylupnt_pybind python-package -j 14 --
 cp -r build/lib/python_package/pylupnt .venv/lib/python3.12/site-packages
 cp build/third_party/AI-Toolbox/AIToolbox.dylib .venv/lib/python3.12/site-packages/AIToolbox.so
 echo "PyLuPNT and AI-Toolbox installed in .venv"
