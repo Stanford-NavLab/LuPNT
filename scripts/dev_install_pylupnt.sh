52b79842feccb79b71ef0be48113e8caf66dce28
Guillem <gcasadesusvila@gmail.com>
2024-04-04 14:23:32 -0700

    Renamed pylupnt to python

--- scripts/dev_install_pylupnt.sh
+++ scripts/dev_install_pylupnt.sh
@@ -1,5 +1,5 @@
 cmake --build build --config Debug --target lupnt pylupnt_pybind python-package -j 14 --
 # cp -r build/lib/python_package/pylupnt .venv/lib/python3.12/site-packages
 # cp build/third_party/AI-Toolbox/AIToolbox.dylib .venv/lib/python3.12/site-packages/AIToolbox.so
-cp build/lib/python_package/pylupnt/pylupnt_pybind.cpython-*-darwin.so pylupnt/pylupnt
+cp build/lib/python_package/pylupnt/pylupnt_pybind.cpython-*-darwin.so python/pylupnt
 echo "PyLuPNT installed in .venv"
