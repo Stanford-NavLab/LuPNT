498218bbe651f337e4aca248ac56dcc601aca51d
Tara Mina <tymina@stanford.edu>
2024-05-21 11:34:00 -0700

    Added coordinate conversion to compute ranges (but commented it, since it seems there's a bug somewhere)

--- examples/python/test_lunarsurfacenetwork.py
+++ examples/python/test_lunarsurfacenetwork.py
@@ -6,6 +6,8 @@ import pylupnt as pnt
 import pylupnt.plots as plots
 import pylupnt.utils as u
 
+from pylupnt import SpiceInterface as sp
+
 # mpl.rcParams.update(mpl.rcParamsDefault)
 
 colors = plots.COLORS
@@ -27,7 +29,7 @@ def get_range(pos_arr1, pos_arr2):
     diff_vect = pos_arr1 - pos_arr2
     return np.linalg.norm(pos_arr1 - pos_arr2, axis=1)
 
-def get_range_and_visibility(pos_arr1, ref_pos_arr2, el_mask_deg=5):
+def get_range_elevation_and_visibility(pos_arr1, ref_pos_arr2, el_mask_deg=5):
     # convert elevation mask to radians
     el_mask_rad = pnt.RAD_PER_DEG * el_mask_deg
 
@@ -43,10 +45,12 @@ def get_range_and_visibility(pos_arr1, ref_pos_arr2, el_mask_deg=5):
 
     # convert unit vector to elevation, azimuth, range
     vis_arr = np.zeros((len(rng_arr)))
+    el_rad_arr = np.zeros((len(rng_arr)))
     for i in range(len(pos_arr1)):
         [az, el, rng] = pnt.cartesian_to_azimuth_elevation_range(ref_pos_arr2, pos_arr1[i,:])
+        el_rad_arr[i] = el
         vis_arr[i] = (el >= el_mask_rad)
-    return rng_arr, vis_arr
+    return rng_arr, el_rad_arr, vis_arr
 
 
 # Define satellite parameters (ELFO, Lunar Pathfinder)
@@ -56,13 +60,19 @@ i = degToRad(54.856)
 Omega = 0
 w = degToRad(86.322)
 M = degToRad(180)
+
+# specify initial time epoch (TAI frame)
+# convert 2030 Oct 1 00:00:00 UTC to TAI
+tai_time = sp.string_to_tai("2030/10/01 00:00:00")
+print('TAI time:', tai_time, type(tai_time))
+
 # nu = degToRad(92.335)
 # M = pnt.true_to_mean_anomaly(nu, e)
 
 oe = np.array([a, e, i, Omega, w, M])
 
 # State
-x_oe = pnt.ClassicalOE(oe, coord_sys=pnt.CoordSystem.MI)
+x_oe = pnt.ClassicalOE(oe, coord_sys=pnt.CoordSystem.MI) # MI -- J200 frame
 print(" ")
 print("Classical orbital elements:")
 print(x_oe.vector)
@@ -79,6 +89,7 @@ print('Orbital period [hrs]:', T/3600)
 # Propagate for certain amount of time and save propagated satellite state
 dt = 1
 t_arr = np.arange(0, 2*T + dt, dt)
+t_arr_tai = tai_time + t_arr 
 len_t_arr = len(t_arr)
 x_oe_arr = np.zeros((len(t_arr), 6))
 x_cart_arr = np.zeros((len(t_arr), 6))
@@ -88,11 +99,13 @@ for i, t in enumerate(t_arr):
     x_oe_arr[i,:] = x_oe.vector
 
     # convert from orbital elements to cartesian
-    x_cart_arr[i,:] = pnt.classical_to_cartesian(x_oe, pnt.MU_MOON).vector
-    # TODO: coordinate transformation here? from inertial to moon-centered inertial
+    # x_cart_arr[i,:] = pnt.classical_to_cartesian(x_oe, pnt.MU_MOON).vector
+    curr_tai_time = t_arr_tai[i]
+    cart_pos = pnt.classical_to_cartesian(x_oe, pnt.MU_MOON).vector
+    x_cart_arr[i,:] = cart_pos #pnt.CoordConverter.convert(curr_tai_time, cart_pos, coord_sys_in=pnt.CoordSystem.MI, coord_sys_out=pnt.CoordSystem.PA)
 
 
-fig_vis.plot(x_cart_arr[0:int(len_t_arr/2),0:3])
+fig_vis.plot(x_cart_arr[0:int(len_t_arr),0:3])
 
 # Add base station
 az_stat_rad = pnt.RAD_PER_DEG * 0
@@ -110,41 +123,49 @@ fig_vis.plot(r_stat_cart_km, '*')
 plt.show()
 
 # 4. Compute the range and range rate from the satellite to the base station
-rng_arr, vis_arr = get_range_and_visibility(x_cart_arr[:, 0:3], r_stat_cart_km)
+rng_arr, el_rad_arr, vis_arr = get_range_elevation_and_visibility(x_cart_arr[:, 0:3], r_stat_cart_km)
+el_deg_arr = el_rad_arr / pnt.RAD_PER_DEG
 
 print(rng_arr.shape)
 print(t_arr.shape)
 
 # Use matplotlib to plot the range and use latex for the labels
 plt.figure()
-plt.plot(t_arr, rng_arr)
+plt.plot(t_arr/3600.0, rng_arr)
 plt.grid()
-plt.xlabel('Time [s]')
+plt.xlabel('Time [hrs]')
 plt.ylabel('Range [km]')
 plt.title('Range to lunar station')
 plt.show()
 
 plt.figure()
-plt.plot(t_arr, vis_arr)
+plt.plot(t_arr/3600.0, vis_arr)
 plt.grid()
-plt.xlabel('Time [s]')
+plt.xlabel('Time [hrs]')
 plt.ylabel('Visibility')
 plt.title('Visibility of lunar station')
 plt.show()
 
-# get times and ranges when visible
+# get times, ranges, and elevations when visible
 t_arr_vis = t_arr[vis_arr == 1]
 rng_arr_vis = rng_arr[vis_arr == 1]
-# print(len(t_arr_vis))
-# print('sum of vis arr', np.sum(vis_arr))
+el_deg_arr_vis = el_deg_arr[vis_arr == 1]
 
 # plot ranges when visible
 plt.plot()
-plt.plot(t_arr_vis, rng_arr_vis)
+plt.plot(t_arr_vis/3600.0, rng_arr_vis, '.')
 plt.grid()
-plt.xlabel('Time [s]')
+plt.xlabel('Time [hrs]')
 plt.ylabel('Range [km]')
 plt.title('Range to lunar station, when visible')
 plt.show()
 
+# plot elevation wrt station when visible
+plt.plot()
+plt.plot(t_arr_vis/3600.0, el_deg_arr_vis, '.')
+plt.grid()
+plt.xlabel('Time [hrs]')
+plt.ylabel('Elevation [deg]')
+plt.title('Elevation to lunar station')
+plt.show()
 
