f80ac48843a7c4735b701fac7e5a01f3e2cbbcef
Guillem <gcasadesusvila@gmail.com>
2024-08-05 22:58:27 -0700

    Added examples

---
+++ examples/cpp/other/example_omp.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <omp.h>
+
+int main() {
+#pragma omp parallel for
+    for (int i = 0; i < 10; i++) {
+        int thread_id = omp_get_thread_num();
+        int num_threads = omp_get_num_threads();
+        std::cout << "Hello from thread " << thread_id << " out of " << num_threads << " threads.\n";
+    }
+    return 0;
+}
