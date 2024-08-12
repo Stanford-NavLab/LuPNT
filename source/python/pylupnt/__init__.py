8908376aba2345c8fff3940dd15caa9977ed7e86
Guillem <gcasadesusvila@gmail.com>
2024-08-12 15:59:48 -0400

    Working on deployment

--- source/python/pylupnt/__init__.py
+++ source/python/pylupnt/__init__.py
@@ -1,21 +1,21 @@
 try:
-    from ._pylupnt import *  # py2 py3 compatible
-
+    # py2 py3 compatible
     from .utils import *
-    from . import plot
     from . import render
     from .math_utils import *
     from . import scenarios
 
+    # Order matters
+    from ._pylupnt import *
+    from . import plot
+
 except ImportError as e:
-    print(e)
-    print("Import failed. Trying as python wheel.")
-
     # this was installed with as a python wheel
-    from _pylupnt import *
-
     from utils import *
-    import plot
     import render
     from math_utils import *
     import scenarios
+
+    # Order matters
+    from _pylupnt import *
+    import plot
