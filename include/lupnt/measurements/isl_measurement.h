d9a4a303541a19fc9139260d60b65abebf173f02
kdricemt <kdricemt@gmail.com>
2024-08-19 19:24:43 -0700

    Updated ISL Measurements

--- include/lupnt/measurements/isl_measurement.h
+++ include/lupnt/measurements/isl_measurement.h
@@ -20,121 +20,139 @@
 
 namespace lupnt {
 
-  enum class IslMeasurementType {
-    OWR,   // One-way range
-    OWRR,  // One-way range rate
-    TWR,   // Two-way range
-    TWRR,  // Two-way range rate
-    DWR,   // Dual one-way range
-    DWRR,  // Dual one-way range rate
-  };
+enum class IslMeasurementType {
+  OWR,   // One-way range
+  OWRR,  // One-way range rate
+  TWR,   // Two-way range
+  TWRR,  // Two-way range rate
+  DWR,   // Dual one-way range
+  DWRR,  // Dual one-way range rate
+};
 
-  struct OneWayLinkParams {
-    CarrierType carrier_type;  // carrier type
-    double B_L_chip;           // tracking loop noise bandwidth
-    double Tc_chip;            // chip duration
-    double m_R;                // modulation index
-  };
+struct LinkParams {
+  double freq;               // carrier frequency [Hz]
+  CarrierType carrier_type;  // carrier type
+  double B_L_chip;           // tracking loop noise bandwidth
+  double Tc;                 // chip duration
+  double B_L_carrier;        // carrier loop noise bandwidth
+  double sigma_y_1s;         // one-sigma range noise [m]
+  double m_R;                // modulation index
+  double T_I;                // Doppler integration time
+  double G;                  //
 
-  struct TwoWayLinkParams {
-    CarrierType carrier_type;
-    double B_L_chip;
-    double Tc_chip;
-    double m_R;
-  };
+  Real hardware_delay = 1e-6;  // hardware delay [s]
+};
 
-  struct DualOneWayLinkParams {
-    CarrierType carrier_type;
-    double B_L_chip;
-    double Tc_chip;
-    double m_R;
-  };
+class IslMeasurement {
+ private:
+  // True Received Epoch
+  Real epoch_rx_true_;
 
-  class IslMeasurement {
-  private:
-    // Register set of agents
-    std::vector<std::shared_ptr<Agent>> agents_;  // set of agents
+  // Transmission (map to satellite index)
+  ITransmission trans_ow_;                // One-way transmission
+  std::vector<ITransmission> trans_tw_;   // Two-way transmission
+  std::vector<ITransmission> trans_dow_;  // Dual one-way transmission
 
-    // Transmission (map to satellite index)
-    ITransmission trans_ow_;                // One-way transmission
-    std::vector<ITransmission> trans_tw_;   // Two-way transmission
-    std::vector<ITransmission> trans_dow_;  // Dual one-way transmission
+  // Parameters
+  LinkParams linkparams_;
 
-    std::pair<int, int> agent_pair_ow_;
-    std::pair<int, int> agent_pair_tw_;
-    std::pair<int, int> agent_pair_dow_;
+  // Flag if the link is generated
+  bool one_way_generated_ = false;
+  bool two_way_generated_ = false;
+  bool dual_one_way_generated_ = false;
 
-    // Parameters
-    OneWayLinkParams ow_params_;
-    TwoWayLinkParams tw_params_;
-    DualOneWayLinkParams dow_params_;
+  // Agents
+  std::vector<std::shared_ptr<Agent>> agents_;
 
-    // Flag if the link is generated
-    bool one_way_generated_ = false;
-    bool two_way_generated_ = false;
-    bool dual_one_way_generated_ = false;
+  // Random seed
+  int seed_ = 0;
 
-    // Random seed
-    int seed_ = 0;
+  // Occultation bodies
+  std::vector<NaifId> occult_bodies_;
+  VecXd occult_alt_;
 
-    // delay in two-way ranging [s]
-    double hardware_delay_ = 1e-6;
+ public:
+  /**
+   * @brief Construct a new Isl Measurement object (one way)
+   *
+   * @param epoch_rx_true  true received epoch
+   * @param tx  transmitter
+   * @param rx  receiver
+   * @param occult_bodies  occulting bodies
+   * @param occult_alt  occultation altitude
+   * @param link_type  link type
+   */
+  IslMeasurement(Real epoch_rx_true, std::shared_ptr<Transmitter> &tx,
+                 std::shared_ptr<Receiver> &rx,
+                 std::vector<NaifId> occult_bodies, VecXd occult_alt,
+                 std::string link_type);
 
-    // Occultation bodies
-    std::vector<NaifId> occult_bodies_;
-    VecXd occult_alt_;
+  /**
+   * @brief Construct a new Isl Measurement object (two way)
+   *
+   * @param epoch_rx_true
+   * @param target
+   * @param rx
+   * @param occult_bodies
+   * @param occult_alt
+   * @param link_type
+   */
+  IslMeasurement(Real epoch_rx_true, std::shared_ptr<Transceiver> &target,
+                 std::shared_ptr<Transceiver> &rx,
+                 std::vector<NaifId> occult_bodies, VecXd occult_alt,
+                 std::string link_type);
 
-  public:
-    IslMeasurement(std::vector<NaifId> occult_bodies, VecXd occult_alt) {
-      occult_bodies_ = occult_bodies;
-      occult_alt_ = occult_alt;
-    }
+  /********************** One way ISL ***************************/
 
-    void AddAgent(std::shared_ptr<Agent> agent) { agents_.push_back(agent); }
-    void SetHardwareDelay(double hardware_delay) { hardware_delay_ = hardware_delay; }
+  /**
+   * @brief Get the One Way Link object
+   *
+   * @param epoch_rx        epoch of the receiver
+   * @param tx              transmitter
+   * @param rx              receiver
+   * @param occult_bodies   occulting bodies
+   * @param occult_alt      occultation altitude
+   * @return ITransmission
+   */
+  void GenerateOneWayLink(Real epoch_rx, std::shared_ptr<Transmitter> &tx,
+                          std::shared_ptr<Receiver> &rx);
 
-    /********************** One way ISL ***************************/
+  VecX GetTrueOneWayISLMeasurement(Real epoch_rx, MatXd H_ow_rx,
+                                   std::vector<IslMeasurementType> meas_types);
 
-    /**
-     * @brief Get the One Way Link object
-     *
-     * @param epoch_rx        epoch of the receiver
-     * @param tx              transmitter
-     * @param rx              receiver
-     * @param occult_bodies   occulting bodies
-     * @param occult_alt      occultation altitude
-     * @return ITransmission
-     */
-    void GenerateOneWayLink(Real epoch_rx, std::shared_ptr<Transmitter> &tx,
-                            std::shared_ptr<Receiver> &rx);
+  VecX GetOneWayIslMeasurement(Real epoch_rx, Vec6 rv_tx, Vec6 rv_rx,
+                               Vec2 clk_tx, Vec2 clk_rx, MatXd H_ow_rx,
+                               Real hardware_delay,
+                               std::vector<IslMeasurementType> meas_types,
+                               bool with_noise);
 
-    VecX GetTrueOneWayISLMeasurement(Real epoch_rx, int agent_id_tx, int agent_id_rx, MatXd H_ow_rx,
-                                     std::vector<IslMeasurementType> meas_types);
+  Real GetOneWayRangeMeasurement(Real epoch_rx, Vec6 rv_tx, Vec6 rv_rx,
+                                 Vec2 clk_tx, Vec2 clk_rx, MatXd &H_ow_rx,
+                                 Real hardware_delay, Body *tx_center_body,
+                                 Body *rx_center_body, bool is_bodyfixed_tx,
+                                 bool is_bodyfixed_rx, bool with_noise);
 
-    VecX GetOneWayIslMeasurement(Real epoch_rx, Vec6 rv_tx, Vec6 rv_rx, Vec2 clk_tx, Vec2 clk_rx,
-                                 MatXd H_ow_rx, Real hardware_delay,
-                                 std::vector<IslMeasurementType> meas_types, bool with_noise);
+  Real GetOneWayRangeRateMeasurement(Real epoch_rx, Vec6 rv_tx, Vec6 rv_rx,
+                                     Vec2 clk_tx, Vec2 clk_rx, MatXd &H_ow_rx,
+                                     Real hardware_delay, Body *tx_center_body,
+                                     Body *rx_center_body, bool is_bodyfixed_tx,
+                                     bool is_bodyfixed_rx, bool with_noise);
 
-    Real GetOneWayRangeMeasurement(Real epoch_rx, Vec6 rv_tx, Vec6 rv_rx, Vec2 clk_tx, Vec2 clk_rx,
-                                   MatXd &H_ow_rx, Real hardware_delay, Body *tx_center_body,
-                                   Body *rx_center_body, bool is_bodyfixed_tx, bool is_bodyfixed_rx,
-                                   bool with_noise);
+  /********************** Two way ISL ***************************/
 
-    /********************** Two way ISL ***************************/
-
-    /**
-     * @brief Generate Two way link
-     *
-     * @param epoch_rx       epoch of the receiver
-     * @param tr1            transceiver 1
-     * @param tr2            transceiver 2
-     * @param occult_bodies  occulting bodies
-     * @param occult_alt     occultation altitude
-     * @return std::vector<ITransmission>
-     */
-    void GenerateTwoWayLink(Real epoch_rx, std::shared_ptr<Transceiver> &tr1,
-                            std::shared_ptr<Transceiver> &tr2, std::vector<NaifId> occult_bodies,
-                            VecXd occult_alt);
-  };
+  /**
+   * @brief Generate Two way link (receiver->target->receiver)
+   *
+   * @param epoch_rx       epoch of the receiver
+   * @param tr_receiver    transceiver 1 (receiver)
+   * @param tr_target      transceiver 2 (target)
+   * @param occult_bodies  occulting bodies
+   * @param occult_alt     occultation altitude
+   * @return std::vector<ITransmission>
+   */
+  void GenerateTwoWayLink(Real epoch_rx,
+                          std::shared_ptr<Transceiver> &tr_receiver,
+                          std::shared_ptr<Transceiver> &tr_target);
+};
 
 }  // namespace lupnt
--- source/cpp/measurements/isl_measurement.cc
+++ source/cpp/measurements/isl_measurement.cc
@@ -1,129 +1,235 @@
 #include "lupnt/measurements/isl_measurement.h"
 
+#include "lupnt/core/constants.h"
 #include "lupnt/measurements/radio_measurement.h"
 #include "lupnt/measurements/space_channel.h"
 
 namespace lupnt {
 
-  /********************** One way ISL ***************************/
-
-  void IslMeasurement::GenerateOneWayLink(Real epoch_rx, std::shared_ptr<Transmitter> &tx,
-                                          std::shared_ptr<Receiver> &rx) {
-    SpaceChannel sc = SpaceChannel();
-    sc.SetOccultationBodies(occult_bodies_, occult_alt_);
-    trans_ow_ = sc.ComputeLinkBudget(tx, rx, epoch_rx.val(), "rx");
-    one_way_generated_ = true;
+IslMeasurement::IslMeasurement(Real epoch_rx_true,
+                               std::shared_ptr<Transmitter> &tx,
+                               std::shared_ptr<Receiver> &rx,
+                               std::vector<NaifId> occult_bodies,
+                               VecXd occult_alt,
+                               std::string link_type = "two_way")
+    : epoch_rx_true_(epoch_rx_true),
+      occult_bodies_(occult_bodies),
+      occult_alt_(occult_alt) {
+  if (link_type == "one_way") {
+    GenerateOneWayLink(epoch_rx_true, tx, rx);
+  } else if (link_type == "two_way") {
+    std::cerr << "Error: Invalid link type, pass transceivers for two-way link"
+              << std::endl;
   }
 
-  VecX IslMeasurement::GetTrueOneWayISLMeasurement(Real epoch_rx, int agent_id_tx, int agent_id_rx,
-                                                   MatXd H_ow_rx,
-                                                   std::vector<IslMeasurementType> meas_types) {
-    VecX rv_tx = agents_[agent_id_tx]->GetRvStateAtEpoch(epoch_rx);
-    VecX rv_rx = agents_[agent_id_rx]->GetRvStateAtEpoch(epoch_rx);
-    VecX clk_tx = agents_[agent_id_tx]->GetClockStateVecAtEpoch(epoch_rx);
-    VecX clk_rx = agents_[agent_id_rx]->GetClockStateVecAtEpoch(epoch_rx);
+  // Register the agents
+  agents_.push_back(tx->GetAgent());
+  agents_.push_back(rx->GetAgent());
+}
 
-    return GetOneWayIslMeasurement(epoch_rx, rv_tx, rv_rx, clk_tx, clk_rx, H_ow_rx, hardware_delay_,
-                                   meas_types, true);
+IslMeasurement::IslMeasurement(Real epoch_rx_true,
+                               std::shared_ptr<Transceiver> &target,
+                               std::shared_ptr<Transceiver> &receiver,
+                               std::vector<NaifId> occult_bodies,
+                               VecXd occult_alt, std::string link_type)
+    : epoch_rx_true_(epoch_rx_true),
+      occult_bodies_(occult_bodies),
+      occult_alt_(occult_alt) {
+  if (link_type == "two_way") {
+    GenerateTwoWayLink(epoch_rx_true, receiver, target);
+  } else if (link_type == "one_way") {
+    std::cerr << "Error: Invalid link type, pass transmitter and receiver for "
+                 "one-way link"
+              << std::endl;
   }
 
-  VecX IslMeasurement::GetOneWayIslMeasurement(Real epoch_rx, Vec6 rv_tx, Vec6 rv_rx, Vec2 clk_tx,
-                                               Vec2 clk_rx, MatXd H_ow_rx, Real hardware_delay,
-                                               std::vector<IslMeasurementType> meas_types,
-                                               bool with_noise) {
-    Real rho_ow, rho_ow_rate;
-    double sigma_ow = 0.0;
-    double sigma_ow_rate = 0.0;
+  // Register the agents
+  agents_.push_back(target->GetAgent());
+  agents_.push_back(receiver->GetAgent());
+}
 
-    int meas_size = meas_types.size();
-    int state_size = 8;
+/********************** One way ISL ***************************/
 
-    VecX z(meas_size);
-    H_ow_rx.resize(meas_size, state_size);
+void IslMeasurement::GenerateOneWayLink(Real epoch_rx,
+                                        std::shared_ptr<Transmitter> &tx,
+                                        std::shared_ptr<Receiver> &rx) {
+  SpaceChannel sc = SpaceChannel();
+  sc.SetOccultationBodies(occult_bodies_, occult_alt_);
+  trans_ow_ = sc.ComputeLinkBudget(tx, rx, epoch_rx.val(), "rx");
+  one_way_generated_ = true;
+}
 
-    int idx = 0;
-    Body tx_center_body = CreateDefaultBody(trans_ow_.tx->GetAgent()->GetBodyId());
-    Body rx_center_body = CreateDefaultBody(trans_ow_.rx->GetAgent()->GetBodyId());
-    bool is_bodyfixed_tx = trans_ow_.tx->GetAgent()->IsBodyFixed();
-    bool is_bodyfixed_rx = trans_ow_.rx->GetAgent()->IsBodyFixed();
+VecX IslMeasurement::GetTrueOneWayISLMeasurement(
+    Real epoch_rx, MatXd H_ow_rx, std::vector<IslMeasurementType> meas_types) {
+  if (!one_way_generated_) {
+    std::cerr << "Error: One way link not generated" << std::endl;
+    return VecX::Zero(0);
+  }
 
-    if (meas_size == 0) {
-      return VecX::Zero(0);
+  VecX rv_tx = agents_[0]->GetRvStateAtEpoch(epoch_rx);
+  VecX rv_rx = agents_[0]->GetRvStateAtEpoch(epoch_rx);
+  VecX clk_tx = agents_[1]->GetClockStateVecAtEpoch(epoch_rx);
+  VecX clk_rx = agents_[1]->GetClockStateVecAtEpoch(epoch_rx);
+
+  return GetOneWayIslMeasurement(epoch_rx, rv_tx, rv_rx, clk_tx, clk_rx,
+                                 H_ow_rx, linkparams_.hardware_delay,
+                                 meas_types, true);
+}
+
+VecX IslMeasurement::GetOneWayIslMeasurement(
+    Real epoch_rx, Vec6 rv_tx, Vec6 rv_rx, Vec2 clk_tx, Vec2 clk_rx,
+    MatXd H_ow_rx, Real hardware_delay,
+    std::vector<IslMeasurementType> meas_types, bool with_noise) {
+  Real rho_ow, rho_ow_rate;
+  double sigma_ow = 0.0;
+  double sigma_ow_rate = 0.0;
+
+  int meas_size = meas_types.size();
+  int state_size = 8;
+
+  VecX z(meas_size);
+  H_ow_rx.resize(meas_size, state_size);
+
+  int idx = 0;
+  Body tx_center_body =
+      CreateDefaultBody(trans_ow_.tx->GetAgent()->GetBodyId());
+  Body rx_center_body =
+      CreateDefaultBody(trans_ow_.rx->GetAgent()->GetBodyId());
+  bool is_bodyfixed_tx = trans_ow_.tx->GetAgent()->IsBodyFixed();
+  bool is_bodyfixed_rx = trans_ow_.rx->GetAgent()->IsBodyFixed();
+
+  if (meas_size == 0) {
+    return VecX::Zero(0);
+  }
+
+  for (auto meas_type : meas_types) {
+    switch (meas_type) {
+      case IslMeasurementType::OWR:
+        rho_ow = GetOneWayRangeMeasurement(
+            epoch_rx, rv_tx, rv_rx, clk_tx, clk_rx, H_ow_rx, hardware_delay,
+            &tx_center_body, &rx_center_body, is_bodyfixed_tx, is_bodyfixed_rx,
+            with_noise);
+        z(idx) = rho_ow;
+        idx++;
+
+      case IslMeasurementType::OWRR:
+        rho_ow_rate = GetOneWayRangeRateMeasurement(
+            epoch_rx, rv_tx, rv_rx, clk_tx, clk_rx, H_ow_rx, hardware_delay,
+            &tx_center_body, &rx_center_body, is_bodyfixed_tx, is_bodyfixed_rx,
+            with_noise);
+        z(idx) = rho_ow_rate;
+        idx++;
     }
-
-    for (auto meas_type : meas_types) {
-      switch (meas_type) {
-        case IslMeasurementType::OWR:
-          rho_ow = GetOneWayRangeMeasurement(epoch_rx, rv_tx, rv_rx, clk_tx, clk_rx, H_ow_rx,
-                                             hardware_delay, &tx_center_body, &rx_center_body,
-                                             is_bodyfixed_tx, is_bodyfixed_rx, with_noise);
-          z(idx) = rho_ow;
-          idx++;
-      }
-    }
-
-    return z;
   }
 
-  Real IslMeasurement::GetOneWayRangeMeasurement(Real epoch_rx, Vec6 rv_tx, Vec6 rv_rx, Vec2 clk_tx,
-                                                 Vec2 clk_rx, MatXd &H_ow_rx, Real hardware_delay,
-                                                 Body *tx_center_body, Body *rx_center_body,
-                                                 bool is_bodyfixed_tx, bool is_bodyfixed_rx,
-                                                 bool with_noise) {
-    // return
+  return z;
+}
 
-    auto func = [epoch_rx, rv_tx, clk_tx, tx_center_body, rx_center_body, is_bodyfixed_tx,
-                 is_bodyfixed_rx, hardware_delay](Vec6 rv_rx_in, Vec2 clk_rx_in) {
-      Real owr = RadioMeasurement::ComputeOneWayRangeLTR(
-          epoch_rx, rv_tx, rv_rx_in, clk_tx(0), clk_rx_in(0), tx_center_body, rx_center_body,
-          is_bodyfixed_tx, is_bodyfixed_rx, hardware_delay);
-      return owr;
-    };
+Real IslMeasurement::GetOneWayRangeMeasurement(
+    Real epoch_rx, Vec6 rv_tx, Vec6 rv_rx, Vec2 clk_tx, Vec2 clk_rx,
+    MatXd &H_ow_rx, Real hardware_delay, Body *tx_center_body,
+    Body *rx_center_body, bool is_bodyfixed_tx, bool is_bodyfixed_rx,
+    bool with_noise) {
+  // return
 
-    // break the computational graph relations before taking the jacobian
-    // Vec6 rv_rx_tmp = rv_rx.cast<double>();
-    // Vec2 clk_rx_tmp = clk_rx.cast<double>();
+  auto func = [epoch_rx, rv_tx, clk_tx, tx_center_body, rx_center_body,
+               is_bodyfixed_tx, is_bodyfixed_rx,
+               hardware_delay](const Vec6 rv_rx_in, const Vec2 clk_rx_in) {
+    Real owr = RadioMeasurement::ComputeOneWayRangeLTR(
+        epoch_rx, rv_tx, rv_rx_in, clk_tx(0), clk_rx_in(0), tx_center_body,
+        rx_center_body, is_bodyfixed_tx, is_bodyfixed_rx, hardware_delay);
+    return owr;
+  };
 
-    Vec6 rv_rx_tmp = rv_rx;
-    Vec2 clk_rx_tmp = clk_rx;
+  // Without lighttime delay
+  // auto func = [rv_tx, clk_tx](const Vec6 rv_rx_in, const Vec2 clk_rx_in) {
+  //   Real owr = RadioMeasurement::ComputePseudorange(
+  //       rv_tx.head(3), rv_rx_in.head(3), clk_tx(0), clk_rx_in(0), 0.0);
+  //   return owr;
+  // };
 
-    Real rho_ow = 0.0;
+  // break the computational graph relations before taking the jacobian
+  Vec6 rv_rx_tmp = rv_rx.cast<double>();
+  Vec2 clk_rx_tmp = clk_rx.cast<double>();
+  Real rho_ow = 0.0;
+  VecXd H_ow_vec = gradient(func, wrt(rv_rx_tmp, clk_rx_tmp),
+                            at(rv_rx_tmp, clk_rx_tmp), rho_ow);
 
-    // H_ow_rx = jacobian(func, wrt(rv_rx_tmp, clk_rx_tmp),
-    //                    at(rv_rx_tmp, clk_rx_tmp), rho_ow);
+  // Convert to (1, 8) matrix
+  H_ow_rx.row(0) = H_ow_vec.transpose();
 
-    if (with_noise) {
-      double sigma_ow = RadioMeasurement::ComputePnRangeErrorCTL(
-          trans_ow_.CN0_linear, ow_params_.B_L_chip, ow_params_.Tc_chip, ow_params_.carrier_type);
-      rho_ow += SampleRandNormal(0.0, sigma_ow, seed_);
-    }
-
-    return rho_ow;
+  if (with_noise) {
+    double sigma_ow = RadioMeasurement::ComputePnRangeErrorCTL(
+        trans_ow_.CN0_linear, linkparams_.B_L_chip, linkparams_.Tc,
+        linkparams_.carrier_type);
+    rho_ow += SampleRandNormal(0.0, sigma_ow, seed_);
   }
 
-  /********************** Two way ISL ***************************/
+  return rho_ow;
+}
 
-  void IslMeasurement::GenerateTwoWayLink(Real epoch_rx, std::shared_ptr<Transceiver> &tr1,
-                                          std::shared_ptr<Transceiver> &tr2,
-                                          std::vector<NaifId> occult_bodies, VecXd occult_alt) {
-    SpaceChannel sc = SpaceChannel();
-    sc.SetOccultationBodies(occult_bodies, occult_alt);
+Real IslMeasurement::GetOneWayRangeRateMeasurement(
+    Real epoch_rx, Vec6 rv_tx, Vec6 rv_rx, Vec2 clk_tx, Vec2 clk_rx,
+    MatXd &H_ow_rx, Real hardware_delay, Body *tx_center_body,
+    Body *rx_center_body, bool is_bodyfixed_tx, bool is_bodyfixed_rx,
+    bool with_noise) {
+  double T_I = linkparams_.T_I;
 
-    // tr2 -> tr1 (downlink)
-    std::shared_ptr<Transmitter> tx_d = tr2->GetTransmitter();
-    std::shared_ptr<Receiver> rx_d = tr1->GetReceiver();
-    ITransmission trans_d = sc.ComputeLinkBudget(tx_d, rx_d, epoch_rx.val(), "rx");
+  auto func = [epoch_rx, rv_tx, clk_tx, tx_center_body, rx_center_body,
+               is_bodyfixed_tx, is_bodyfixed_rx, T_I,
+               hardware_delay](const Vec6 rv_rx_in, const Vec2 clk_rx_in) {
+    Real owrr = RadioMeasurement::ComputeOneWayRangeRateLTR(
+        epoch_rx, rv_tx, rv_rx_in, clk_tx(1), clk_rx_in(1), tx_center_body,
+        rx_center_body, is_bodyfixed_tx, is_bodyfixed_rx, hardware_delay, T_I);
+    return owrr;
+  };
 
-    // tr1 -> tr2 (uplink)
-    Real epoch_rx_u = trans_d.t_tx;
-    std::shared_ptr<Transmitter> tx_u = tr1->GetTransmitter();
-    std::shared_ptr<Receiver> rx_u = tr2->GetReceiver();
-    ITransmission trans_u = sc.ComputeLinkBudget(tx_u, rx_u, epoch_rx_u, "rx");
+  // break the computational graph relations before taking the jacobian
+  Vec6 rv_rx_tmp = rv_rx.cast<double>();
+  Vec2 clk_rx_tmp = clk_rx.cast<double>();
+  Real rho_ow_rate = 0.0;
+  VecXd H_ow_rate_vec = gradient(func, wrt(rv_rx_tmp, clk_rx_tmp),
+                                 at(rv_rx_tmp, clk_rx_tmp), rho_ow_rate);
 
-    trans_tw_.push_back(trans_u);
-    trans_tw_.push_back(trans_d);
+  // Convert to (1, 8) matrix
+  H_ow_rx.row(1) = H_ow_rate_vec.transpose();
 
-    two_way_generated_ = true;
+  if (with_noise) {
+    double sigma_ow_rate = RadioMeasurement::ComputeRangeRateErrorOneWay(
+        linkparams_.B_L_carrier, linkparams_.freq, linkparams_.Tc,
+        linkparams_.T_I, trans_ow_.CN0_linear, linkparams_.sigma_y_1s,
+        linkparams_.carrier_type, linkparams_.m_R);
+
+    rho_ow_rate += SampleRandNormal(0.0, sigma_ow_rate, seed_);
   }
+
+  return rho_ow_rate;
+}
+
+/********************** Two way ISL ***************************/
+
+void IslMeasurement::GenerateTwoWayLink(
+    Real epoch_rx, std::shared_ptr<Transceiver> &tr_receiver,
+    std::shared_ptr<Transceiver> &tr_target) {
+  SpaceChannel sc = SpaceChannel();
+  sc.SetOccultationBodies(occult_bodies_, occult_alt_);
+
+  // tr_target -> tr_receiver (downlink)
+  std::shared_ptr<Transmitter> tx_d = tr_target->GetTransmitter();
+  std::shared_ptr<Receiver> rx_d = tr_receiver->GetReceiver();
+  ITransmission trans_d =
+      sc.ComputeLinkBudget(tx_d, rx_d, epoch_rx.val(), "rx");
+
+  // tr_receiver -> tr_target (uplink)
+  Real epoch_rx_u = trans_d.t_tx;
+  std::shared_ptr<Transmitter> tx_u = tr_receiver->GetTransmitter();
+  std::shared_ptr<Receiver> rx_u = tr_target->GetReceiver();
+  ITransmission trans_u = sc.ComputeLinkBudget(tx_u, rx_u, epoch_rx_u, "rx");
+
+  trans_tw_.push_back(trans_u);
+  trans_tw_.push_back(trans_d);
+
+  two_way_generated_ = true;
+}
 
 }  // namespace lupnt
