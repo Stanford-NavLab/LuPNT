bb8cfe4a7e796c6523df52924cbd979c76a9230c
Guillem <gcasadesusvila@gmail.com>
2024-08-30 13:45:01 -0700

    Workflows

--- examples/cpp/ex_load_save.cc
+++ examples/cpp/ex_load_save.cc
@@ -1,13 +1,15 @@
 #include <lupnt/lupnt.h>
 
+#include <highfive/H5Easy.hpp>
+
 using namespace lupnt;
 int main() {
-  File file("ex_load_save.h5", File::Overwrite);
+  H5Easy::File file("ex_load_save.h5", H5Easy::File::Overwrite);
 
   MatX A = MatX::Random(10, 5);
   std::cout << A << std::endl << std::endl;
-  dump(file, "/path/to/A", A.cast<double>());
+  H5Easy::dump(file, "/path/to/A", A.cast<double>());
 
-  MatX B = load<MatXd>(file, "/path/to/A");
+  MatX B = H5Easy::load<MatXd>(file, "/path/to/A");
   std::cout << B << std::endl;
 }
